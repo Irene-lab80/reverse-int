f033284a5dac8b3d61df278bb1f7739c7f276514
IrinaZ <you@example.com>
2020-10-25 19:29:00 +0300

    tasks done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if(Number.isInteger(Number(n))) {
+        const str = Math.abs(n).toString();
+        const result = str.split('').reverse().join('');
+        return result;
+    }
 }
